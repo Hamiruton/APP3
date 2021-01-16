c7cec61d147f562755f815424f42c673957f09bc
Axel Hamilton <axelhamilton02@gmail.com>
2021-01-16 10:54:48 +0100

    Hasher le mot de passe lors de l'inscription

--- routes/inscription.js
+++ routes/inscription.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const schema = require('../tools/validate').schema_inscription;
+const hasher = require('../tools/hash_psw');
 const Data = require('../models/data');
 
 router.get('/inscrire', (req, res)=>{
@@ -9,12 +10,17 @@ router.get('/inscrire', (req, res)=>{
 router.post('/inscrire', async (req, res)=>{
     const infos = await schema.validateAsync(req.body);
     //hasher le mot de passe
-    //enregistrer les données dans la BD
-    const inscription = new Data(infos);
-    inscription.inscrire().then(()=>{
-        //
-    }).catch((err)=>{
-        console.error(err);
+    hasher(infos.psw).then(hash=>{
+        infos.psw = hash;
+        //enregistrer les données dans la BD
+        const inscription = new Data(infos);
+        inscription.inscrire().then(()=>{
+            //
+        }).catch((err)=>{
+            console.error(err);
+        });
+    }).catch(err=>{
+        console.log(err);
     });
 });
 
